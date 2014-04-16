a87a74d98ceb0642b6022913fbf8ab0241319ad9
vincent rotter & jeremy clemans <student@epicodus.com>
2014-04-16 13:57:29 -0700

    add creation of rentals for a user

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,4 +1,17 @@
 class RentalsController < ApplicationController
   def create
+    @user = User.find(params[:rental][:user_id])
+    @rental = @user.rentals.new(rental_params)
+    if @rental.save
+      redirect_to root_path
+    else
+      render 'users/show'
+    end
+  end
+
+
+private
+  def rental_params
+    params.require(:rental).permit(:description, :location, :people)
   end
 end
