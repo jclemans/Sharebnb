05969604cff67b6a462cb1c31fab6f04dd13116d
vincent rotter & jeremy clemans <student@epicodus.com>
2014-04-16 09:12:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sharebnb::Application.config.secret_key_base = '10430d5061dc01208e100044c01af777dc62631dafbbac67f32c640bcd1923ba8c0bae83cdc66ba7c24c0f35110a5e869b53189df76215e92d859239462e706a'
